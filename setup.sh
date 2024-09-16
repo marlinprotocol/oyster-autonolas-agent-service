e7de199d3648a9c7b33fcf5c4f74260f92ec9d8b
AyushKaul <ayushk.kaul3@gmail.com>
2024-09-16 21:11:44 +0530

    added iptable rules

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ set -e
 ifconfig lo 127.0.0.1
 ip addr
 
+echo "127.0.0.1 localhost" > /etc/hosts
+
 # adding a default route
 ip route add default dev lo src 127.0.0.1
 ip route
@@ -15,6 +17,12 @@ iptables -t nat -N DOCKER
 iptables -t nat -A OUTPUT -d 172.17.0.0/16 -j DOCKER
 iptables -A OUTPUT -t nat -p tcp --dport 1:65535 ! -d 127.0.0.1  -j DNAT --to-destination 127.0.0.1:1200
 iptables -L -t nat
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+
+# Allow HTTP and HTTPS traffic
+iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
 # generate identity key
 /app/keygen-ed25519 --secret /app/id.sec --public /app/id.pub
